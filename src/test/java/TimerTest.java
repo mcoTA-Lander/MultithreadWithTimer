8e2cc409950e170329055b3359a2e774a7d5fc59
mcoTA-Lander <77455780+mcoTA-Lander@users.noreply.github.com>
2021-03-10 01:02:59 -0500

    Update TimerTest.java

--- src/test/java/TimerTest.java
+++ src/test/java/TimerTest.java
@@ -5,20 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 //ANY EDITS WILL RESULT IN POINT REDUCTION
 
 class TimerTest {
-
-
     Timer t;
     
     @Test
      void TestStart() {
          t = new Timer();
-         t.start();
-         Exception e = assertThrows(Exception.class, () -> t.start());
+        try {
+            t.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Exception e = assertThrows(Exception.class, () -> t.start());
          assertEquals("Timer Already Started", e.getMessage());
      }
  
      @Test
-     void TestStop() {
+     void TestStop() throws Exception {
          long duration;
          t = new Timer();
          t.start();
@@ -28,7 +30,7 @@ class TimerTest {
      }
  
      @Test
-     void TestReset() {
+     void TestReset() throws Exception {
          t = new Timer();
          t.start();
          t.reset();
